b384a1069cfe2ce8be4435c5fe0d01f0a2ef0a41
Kyle McCahill <kylemccahill@gmail.com>
2023-03-26 21:06:27 -0500

    added distance scaling, now the background representation is true but the visual is scaled

--- src/main.c
+++ src/main.c
@@ -22,8 +22,9 @@
 
 struct OrbitData
 {
-    float bodyDistance;
-    float attractiveForce; // newtons universal grav
+    double bodyDistance;
+    // double attractiveForce; // newtons universal grav
+    Vector2 scaledPosition;
 };
 
 struct OrbitLine {
@@ -55,6 +56,9 @@ int main(void)
     // Initialization
     const int screenWidth = 1920;
     const int screenHeight = 1080;
+    
+    const double simScale = 10000000; // from some example of one au in screen scale size, for rendering.
+    const Vector2 simScaleVector = {simScale, simScale};
 
     InitWindow(screenWidth, screenHeight, "raylib simple orbital sim");
 
@@ -65,32 +69,28 @@ int main(void)
     camera.zoom = 1.0f;
     camera.rotation = 0.0f;
 
-    float cameraSpeed = 10.0f;
+    float cameraSpeed = 5000.0f;
     float camera_zoom_max = 5.0f;
     float camera_zoom_min = 0.1f;
     
     // Star -- function
     Vector2 starPosition = { 0, 0 };
-    Vector2 starVelocity = { (float) 0, (float)0 };
-    // float starMass = 333000;
-    // float starMass = 333000000;
-    float starMass = 3330000000;
-    float starRadius = 64;
+    Vector2 starVelocity = { (double) 0, (double)0 };
+    double starMass = 1.9891 * powf(10, 30);  // actual sun mass
+    double starRadius = 64;
     struct Body star = {starPosition, starVelocity, starMass, starRadius};
 
     // Orbiting Body -- function
-    Vector2 bodyPosition = { 1000, 0 };
-    // Vector2 bodyVelocity = { (float)-0.00, (float)-0.003 }; // avg earth 29,784.8 m/s
-    // Vector2 bodyVelocity = { (float)-0.00, (float)-0.03 }; // avg earth 29,784.8 m/s
-    Vector2 bodyVelocity = { (float)-0.00, (float)-0.02 }; // avg earth 29,784.8 m/s
-    float bodyMass = 1;
-    float bodyRadius = 5;
+    Vector2 bodyPosition = { 150000000000, 0 };
+    Vector2 bodyVelocity = { (double)-0.00, (double)-29784.0 }; // avg earth 29,784.8 m/s
+    double bodyMass = 5.9722 * powf(10, 24);  // actual earth mass
+    double bodyRadius = 15;
     struct Body body = {bodyPosition, bodyVelocity, bodyMass, bodyRadius};
 
-    struct OrbitData data = { distance_to_body(body.position, star.position) };
+    Vector2 scaledPosition = Vector2Divide(body.position, simScaleVector);
+    struct OrbitData data = { distance_to_body(body.position, star.position), scaledPosition };
 
     struct OrbitLine orbit_line = {4096, 0};
-
 
     // Important stuff
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -99,14 +99,14 @@ int main(void)
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
+        float delta = GetFrameTime(); // for somoother movement
 
         // Camera Movement
-        if (IsKeyDown(KEY_A)) camera.target.x -= cameraSpeed;
-        else if (IsKeyDown(KEY_D)) camera.target.x += cameraSpeed;
+        if (IsKeyDown(KEY_A)) camera.target.x -= cameraSpeed * delta;
+        else if (IsKeyDown(KEY_D)) camera.target.x += cameraSpeed * delta;
 
-        if (IsKeyDown(KEY_W)) camera.target.y -= cameraSpeed;
-        else if (IsKeyDown(KEY_S)) camera.target.y += cameraSpeed;
-
+        if (IsKeyDown(KEY_W)) camera.target.y -= cameraSpeed * delta;
+        else if (IsKeyDown(KEY_S)) camera.target.y += cameraSpeed * delta;
 
         // camera zoom
         camera.zoom += ((float)GetMouseWheelMove()*0.05f);
@@ -120,35 +120,44 @@ int main(void)
         // Body movement
         body.velocity = caluclate_orbit_vector(body, star);
 
-        body.position.y += body.velocity.y;
-        body.position.x += body.velocity.x;
+        // body.position = Vector2Scale(Vector2Add(body.position, body.velocity), delta);
+        body.position.y = body.position.y + body.velocity.y * delta;
+        body.position.x = body.position.x + body.velocity.x * delta;
 
 
         // data update
         data.bodyDistance = distance_to_body(body.position, star.position);
+        data.scaledPosition = Vector2Divide(body.position, simScaleVector);
 
         // orbit line update
         bodyPosition = body.position;
-        if ((int)bodyPosition.y % 5 == 0) add_orbit_point(&orbit_line, body.position);
+        if ((int)bodyPosition.y % 5 == 0) add_orbit_point(&orbit_line, data.scaledPosition);
         // Draw
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
-            // world sorta
+            // Draw simulated bodies
             BeginMode2D(camera);
                 DrawCircleV(star.position, star.radius, YELLOW); // truescale 695
-                DrawCircleV(body.position, body.radius, BLUE); // truescale 6
+                DrawCircleV(data.scaledPosition, body.radius, BLUE); // truescale 6
                 DrawLineV(body.position, Vector2Add(body.position, Vector2Scale(body.velocity, 200)), RED); // draws a line showing velocity vector of body
                 draw_orbit_line(orbit_line);
             EndMode2D();
             
-            // DrawText(TextFormat("Star Mass: %.10e", star.mass), 20, 50, 28, BLACK);
-            // DrawText(TextFormat("Body Mass: %.10e", body.mass), 20, 70, 28, BLACK);
-            DrawText(TextFormat("Star Mass: %.2f kg", star.mass), 20, 50, 28, BLACK);
-            DrawText(TextFormat("Body Mass: %.2f kg", body.mass), 20, 80, 28, BLACK);
+
+            // UI --- Orbital Information
+            DrawText(TextFormat("Star Mass: %.4e kg", star.mass), 20, 50, 28, BLACK);
+            DrawText(TextFormat("Body Mass: %.4e kg", body.mass), 20, 80, 28, BLACK);
             DrawText(TextFormat("Body Distance: %.3f", data.bodyDistance), 20, 110, 28, BLACK);
             DrawText(TextFormat("Body Velocity X: %.8f", body.velocity.x), 20, 140, 28, BLACK);
             DrawText(TextFormat("Body Velocity Y: %.8f", body.velocity.y), 20, 170, 28, BLACK);
+
+            // UI --- Camera Information
+            DrawText(TextFormat("Camera Zoom: %.1f", camera.zoom), 20, 970, 28, BLACK);
+            DrawText(TextFormat("Camera Position: %.1f, %.1f", camera.target.x, camera.target.y), 20, 1000, 28, BLACK);
+
+            // UI -- Simulation Information.
+            DrawText(TextFormat("Time Scale: %.1fx", 1.0), 1600, 1000, 28, BLACK);
 
 
         EndDrawing();
