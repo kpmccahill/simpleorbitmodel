0fa0e1256137f33936581ba55340fcd6b27c52d6
Kyle McCahill <kylemccahill@gmail.com>
2024-02-22 23:08:56 -0600

    updated build system, and to raylib 5.0

---
+++ build/CMakeFiles/dungeoncrawler.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dungeoncrawler.dir/src/body.c.o"
+  "CMakeFiles/dungeoncrawler.dir/src/body.c.o.d"
+  "CMakeFiles/dungeoncrawler.dir/src/main.c.o"
+  "CMakeFiles/dungeoncrawler.dir/src/main.c.o.d"
+  "dungeoncrawler"
+  "dungeoncrawler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dungeoncrawler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/dungeoncrawler.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dungeoncrawler.
+# This may be replaced when dependencies are built.
